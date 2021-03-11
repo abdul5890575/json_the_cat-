09d213a929f6a97000d6e26c5c924fb4c9bcd845
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-11 14:46:50 +0000

    request

---
+++ breedFetcher.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+let args = process.argv;
+const ENDPOINT = 'https://api.thecatapi.com/v1/breeds/search';
+const params = `?q=${args[2]}`;
+
+if (args[2] === undefined) {
+  console.log('Please provide a breed');
+    
+} else {
+  request(ENDPOINT + params, (error, response, body) => {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    //console.log('body:', body);
+    const data = JSON.parse(body);
+    try {
+      console.log(data[0]['description']); //first entry of weight object
+    } catch (err) {
+      console.log('Breed does not exist in database');
+    }
+  });
+}
+
+
