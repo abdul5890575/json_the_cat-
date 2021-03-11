9be9568122797d56a4e42dc58cc9f9b95ec87ee6
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-11 19:02:51 +0000

    refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,25 @@
 const request = require('request');
 
-let args = process.argv;
 const ENDPOINT = 'https://api.thecatapi.com/v1/breeds/search';
-const params = `?q=${args[2]}`;
 
-if (args[2] === undefined) {
-  console.log('Please provide a breed');
-    
-} else {
-  request(ENDPOINT + params, (error, response, body) => {
-    if(error) {
-      console.log('error:', error);
-      console.log('statusCode:', response && response.statusCode);
-    } else {
-        //console.log('body:', body);
-      const data = JSON.parse(body);
-      try {
-        console.log(data[0]['description']); //first entry of weight object
-      } catch (err) {
-        console.log('Breed does not exist in database');
+const fetchBreedDescription = (breedName, callback) => {
+    console.log(breedName)
+    if (breedName === undefined) {
+      callback('No breed defined', null);
+  } else {
+      request(ENDPOINT + '?q=' + breedName, (error, response, body) => {
+      if(error) {
+        callback(error,null);
+      } else {
+        const data = JSON.parse(body);
+        try {
+          callback(null,data[0]['description']);
+        } catch (err) {
+          callback('Breed does not exist in database',null);
+        }
       }
-    }
-  });
-}
+    });
+  }
+};
 
-
+module.exports = fetchBreedDescription;
